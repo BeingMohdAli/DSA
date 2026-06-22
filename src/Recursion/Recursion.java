0235fcc9c23fcc7eaf12e93cee1e0ec9a69c184e
BeingMohdAli <mohdalisaad868@gmail.com>
2026-06-22 10:53:01 +0530

    recursion

--- src/Recursion/Recursion.java
+++ src/Recursion/Recursion.java
@@ -2,7 +2,18 @@ package Recursion;
 
 public class Recursion {
 
+    static void main() {
+        num(5);
 
+    }
+
+    public static void num(int n ){
+       if(n==0){
+           return;
+       }
+       num(n-1);
+        System.out.println(n);
+    }
 
 }
 
