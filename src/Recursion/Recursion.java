a9b21f17cc5def61b237053ce7abcb27107aa618
BeingMohdAli <mohdalisaad868@gmail.com>
2026-06-24 11:15:23 +0530

    recursion

--- src/Recursion/Recursion.java
+++ src/Recursion/Recursion.java
@@ -4,59 +4,85 @@ public class Recursion {
 
     static void main() {
 
-        System.out.println(sumDigits(142507));
+        System.out.println(revNum(1));
     }
 
 
-    public static void num(int n ){
-       if(n==0){
-           return;
-       }
-       num(n-1);
+    public static void num(int n) {
+        if (n == 0) {
+            return;
+        }
+        num(n - 1);
         System.out.println(n);
     }
-    public static void numRev(int n){
-        if(n==0){
+
+    public static void numRev(int n) {
+        if (n == 0) {
             return;
 
         }
         System.out.println(n);
-        numRev(n-1);
+        numRev(n - 1);
 
 
     }
 
-    public static void printBoth(int n){
-        if(n==0){
+    public static void printBoth(int n) {
+        if (n == 0) {
             return;
         }
         System.out.println(n);
-        printBoth(n-1);
+        printBoth(n - 1);
         System.out.println(n);
     }
 
-    public static int fact(int n){
-     if(n==1||n==0){
-         return 1;
-     }
-     return n* fact(n-1);
+    public static int fact(int n) {
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        return n * fact(n - 1);
     }
 
-    public static int sum(int n){
-        if(n==1){
+    public static int sum(int n) {
+        if (n == 1) {
             return 1;
         }
 
-        return n+sum(n-1);
+        return n + sum(n - 1);
     }
 
-    public static int sumDigits(int n ){
-        if(n/10<1){
+    public static int sumDigits(int n) {
+        if (n / 10 < 1) {
             return n;
 
         }
 
-        return n % 10 + sumDigits(n/10);
+        return n % 10 + sumDigits(n / 10);
     }
+
+//    public static int printingReverseNum(int n){
+//        if(n==0){
+//            return 0;
+//        }
+//        System.out.print(n%10);
+//        return reverseNum(n/10) ;
+//    }
+
+    public static int revNum(int n){
+
+        int digit = ((int)Math.log10(n)) +1;
+        return helper(n , digit);
+    }
+
+    private static int helper(int n, int digit) {
+        if((n/10)<1){
+            return n ;
+        }
+        return  (n%10) *(int) Math.pow(10,digit -1) + helper(n/10,digit-1);
+    }
+
+
 }
+
+
 
