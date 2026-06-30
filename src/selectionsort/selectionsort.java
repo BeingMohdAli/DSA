9b87515aaaf316a1c4dfa74c4ce00c0f76bbea86
BeingMohdAli <mohdalisaad868@gmail.com>
2026-06-30 16:07:15 +0530

    dsa

--- src/selectionsort/selectionsort.java
+++ src/selectionsort/selectionsort.java
@@ -1,4 +1,27 @@
 package selectionsort;
 
+import java.util.Arrays;
+
 public class selectionsort {
+    static void main() {
+        int arr[]  = {8,6,3,2,5,4};
+
+        System.out.println(selectionSort(arr));
+
+    }
+    public static  String selectionSort(int[] arr){
+
+        for (int i = 0; i < arr.length-1; i++) {
+            int k =i;
+            for (int j = i; j < arr.length; j++) {
+                if(arr[j]<arr[k]){
+                    k = j;
+                }
+            }
+            int temp = arr[k];
+            arr[k] = arr[i];
+            arr[i] = temp;
+        }
+        return Arrays.toString(arr);
+    }
 }
