80f455773d762e278f54b4d8fc2506165af60b53
BeingMohdAli <mohdalisaad868@gmail.com>
2026-06-30 16:14:29 +0530

    dsa

--- src/selectionsort/selectionsort.java
+++ src/selectionsort/selectionsort.java
@@ -13,7 +13,7 @@ public class selectionsort {
 
         for (int i = 0; i < arr.length-1; i++) {
             int k =i;
-            for (int j = i; j < arr.length; j++) {
+            for (int j = i+1; j < arr.length; j++) {
                 if(arr[j]<arr[k]){
                     k = j;
                 }
