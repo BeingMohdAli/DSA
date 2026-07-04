f5e341b7f9b9bc136923fcb874f45748a77cbb90
BeingMohdAli <mohdalisaad868@gmail.com>
2026-07-04 23:33:55 +0530

    dsa

--- src/recursionstring.java
+++ src/recursionstring.java
@@ -1,2 +1,24 @@
 public class recursionstring {
+
+    static void main() {
+        System.out.println(fun("dffdsaaada"));
+    }
+
+    static String fun(String s){
+    if(s.isEmpty()){
+        return "";
+
+    }
+
+    char a  = s.charAt(0);
+
+    if(a=='a'){
+       return fun(s.substring(1));
+    }
+
+    return a + fun(s.substring(1));
+
+
+    }
+
 }
