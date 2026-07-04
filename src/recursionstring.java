f559bbfdfb19ffea238e764d16411f65ba54d910
BeingMohdAli <mohdalisaad868@gmail.com>
2026-07-04 23:41:55 +0530

    dsa

--- src/recursionstring.java
+++ src/recursionstring.java
@@ -5,6 +5,8 @@ public class recursionstring {
     }
 
     static String fun(String s){
+//        use .isEmpty() always if checking whatever the string is empty or not means 0 length like this ""
+//        .isEmpty() will be false if string is equal to this " " because length is not zero
     if(s.isEmpty()){
         return "";
 
@@ -15,7 +17,7 @@ public class recursionstring {
     if(a=='a'){
        return fun(s.substring(1));
     }
-
+//#this concatenation will work because this whole thing will become string char + str = str
     return a + fun(s.substring(1));
 
 
