a641f6588b232ef542f5057923a6c844e5ffa19f
BeingMohdAli <mohdalisaad868@gmail.com>
2026-07-06 21:06:49 +0530

    dsa

--- src/recursionstring.java
+++ src/recursionstring.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class recursionstring {
 
     static void main() {
-        System.out.println(fun("dffdsaaada"));
+        System.out.println(permutation("abc"));
     }
 
     static String fun(String s){
@@ -21,6 +24,25 @@ public class recursionstring {
     return a + fun(s.substring(1));
 
 
+    }
+
+
+    static List<String> permutation(String s){
+        if(s.length()==1){
+            List<String> list = new ArrayList<>();
+            list.add(s);
+            return list;
+
+        }
+
+        char a = s.charAt(0);
+        List<String> small = permutation(s.substring(1));
+        List<String> ans = new ArrayList<>();
+        for(String i:small){
+            ans.add(a+i);
+            ans.add(i+a);
+        }
+        return ans ;
     }
 
 }
