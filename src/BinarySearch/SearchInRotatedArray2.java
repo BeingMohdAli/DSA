395362ec414d0b4f14c062edb05138aeca0f1d18
BeingMohdAli <mohdalisaad868@gmail.com>
2026-08-04 11:07:43 +0530

    DSA

--- src/BinarySearch/SearchInRotatedArray2.java
+++ src/BinarySearch/SearchInRotatedArray2.java
@@ -4,57 +4,63 @@ public class SearchInRotatedArray2 {
 
     static void main() {
         int[] arr = {1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1};
+        int target = 2;
 
-        System.out.println(minimumElement(arr));
+        System.out.println(elementInRotatedSortedArray2(arr,target));
     }
-//    public static boolean elementInRotatedSortedArray2(int[] arr,int target){
-//        if(arr.length==1){
-//            return arr[0]==target;
-//        }
-//        int start ,end;
-//        int minElement = minimumElement(arr);
-//        if(minElement==0){
-//            if(target>arr[minElement]){
-//                start = minElement + 1;
-//                 end = arr.length-1;
-//            }
-//        }
-//        else if(target>=arr[0]&&target<=arr[minElement-1]){
-//            start = 0;
-//            end = minElement-1;
-//
-//        }else{
-//            start = minElement;
-//            end = arr.length-1;
-//        }
-//
-//        while(start<=end){
-//            int mid = start + (end-start)/2;
-//            if(arr[mid]==target){
-//                return true;
-//            } else if (arr[mid]>target) {
-//                end = mid -1;
-//
-//            }else{
-//                start = mid + 1;
-//            }
-//        }
-//        return false;
-//    }
+    public static boolean elementInRotatedSortedArray2(int[] arr,int target){
+        if(arr.length==1){
+            return arr[0]==target;
+        }
+        int start,end;
 
-    public static int minimumElement(int[] arr){
-        int start = 0;
-        int end = arr.length-1;
-        while(start<end){
-            int mid = start + (end - start)/2;
-            if(arr[mid]>arr[end]){
-                start = mid +1;
-            }else if(arr[mid]<arr[end]) {
-                end = mid;
+        int minElement = minimumElement(arr);
+        if(minElement==0){
+            start =0;
+            end = arr.length-1;
+
+        }
+
+      else if(target>=arr[0]&&target<=arr[minElement-1]){
+          start = 0;
+          end = minElement-1;
+      }
+    else{
+            start = minElement;
+            end = arr.length-1;
+        }
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]==target){
+                return true;
+            } else if (arr[mid]>target) {
+                end = mid -1;
             }else{
-                end --;
+                start = mid + 1;
             }
         }
-        return start ;
+        return false;
+    }
+
+    public static int minimumElement(int[] arr){
+        int left = 0;
+        int right = arr.length-1;
+        while(left<right&&arr[left]==arr[left+1]){
+            left++;
+        }
+        while(left<right&&arr[right]==arr[right-1]){
+            right--;
+        }
+
+        while(left<right){
+            int mid = left + (right - left)/2;
+            if(arr[mid]>arr[right]){
+                left = mid +1;
+            }else {
+                right = mid;
+            }
+        }
+        return left ;
     }
 }
