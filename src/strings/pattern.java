6e99a1abbed724aae86515cf5c91752d1e8578c0
BeingMohdAli <mohdalisaad868@gmail.com>
2026-08-07 23:38:53 +0530

    DSA

--- src/strings/pattern.java
+++ src/strings/pattern.java
@@ -1,5 +1,7 @@
 package strings;
 
+import java.util.Arrays;
+
 public class pattern{
     /*   * * * *
     /*   * * *
@@ -7,15 +9,50 @@ public class pattern{
     /*   *
      */
     static void main() {
-        int rowLength = 4;
-        int colLength = 4;
-        for (int row = 1; row <=rowLength; row++) {
-            for (int col = 1; col <=colLength ; col++) {
-                System.out.print("* ");
+//        int rowLength = 4;
+//        int colLength = 4;
+//        for (int row = 1; row <=rowLength; row++) {
+//            for (int col = 1; col <=colLength ; col++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println(
+//            );
+//            colLength--;
+
+
+//        String str = "  Hello    World   Java  ";
+//
+//        String[] words = str.trim().split("\\s+");
+//
+//        System.out.println(Arrays.toString(words));
+//       int start = 0;
+//       int end = words.length-1;
+//       while(start<end){
+//           String temp = words[start];
+//           words[start] = words[end];
+//           words[end] = temp;
+//           start++;
+//           end--;
+
+
+
+//       }
+//        System.out.println(Arrays.toString(words));
+//       String s = new String(words);
+
+        String s = "Hello world";
+        String str = "";
+        for (int i = s.length()-1; i >=0 ; i--) {
+            str = s.charAt(i) + str;
+            if(s.charAt(i)==' '){
+                continue;
             }
-            System.out.println(
-            );
-            colLength--;
+
+
         }
+
+        System.out.println(str);
+
     }
-}
+    }
+
