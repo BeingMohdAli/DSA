3c5b6f9757defa2524bed66515af3240bbc13e3d
BeingMohdAli <mohdalisaad868@gmail.com>
2026-08-11 17:02:37 +0530

    DSA

---
+++ src/strings/validanagramlc242.java
@@ -0,0 +1,23 @@
+package strings;
+
+public class validanagramlc242 {
+//    class Solution {
+//        public boolean isAnagram(String s, String t) {
+//
+//            char[] s1 = s.toCharArray();
+//            char[] s2 = t.toCharArray();
+//            Arrays.sort(s1);
+//            Arrays.sort(s2);
+//            String ans1 = new String(s1);
+//            String ans2 = new String(s2);
+//
+//            if(ans1.equals(ans2)){
+//                return true;
+//            }
+//
+//            return false;
+//
+//
+//        }
+//    }
+}
