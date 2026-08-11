9e9122d40e2f41bb63f6c76fe1ad17b6d8936f04
BeingMohdAli <mohdalisaad868@gmail.com>
2026-08-11 17:04:34 +0530

    DSA

---
+++ src/strings/RotateStringLC14.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class RotateStringLC14 {
+//    class Solution {
+//        public boolean rotateString(String s, String goal) {
+//            if(s.length()!=goal.length()){
+//                return false;
+//
+//            }
+//
+//            return (s+s).contains(goal);
+}
