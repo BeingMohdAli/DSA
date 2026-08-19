9845d0594a5e9558055ebfe0a2717a076ead1e6d
BeingMohdAli <mohdalisaad868@gmail.com>
2026-08-19 22:32:51 +0530

    DSA

--- src/Recursion/RverseANumber.java
+++ src/Recursion/RverseANumber.java
@@ -2,18 +2,21 @@ package Recursion;
 
 public class RverseANumber {
     static void main() {
-        System.out.println(reverse(2345));
+        System.out.println(reverse(-2345));
     }
 
     public static int reverse(int x) {
         if ((x / 10) == 0) {
             return x;
         }
+        if(x<0){
+          return -reverse(-x);
+        }
 
-        String s = "";
+
         int lastDigit = x % 10;
         int i = reverse(x / 10);
-        String s1 = s + lastDigit + i;
+        String s1 = "" + lastDigit + i;
         return Integer.parseInt(s1);
 
     }
