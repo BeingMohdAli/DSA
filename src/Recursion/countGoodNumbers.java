4b2ed1a123a346561642dc4666d05908b78ca74a
BeingMohdAli <mohdalisaad868@gmail.com>
2026-08-20 10:15:22 +0530

    DSA

---
+++ src/Recursion/countGoodNumbers.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class countGoodNumbers {
+    static void main() {
+
+
+    }
+
+    public static int goodNumbers(int n){
+        int digits  =  (int)Math.log10(n)+1;
+        return x;
+    }
+}
