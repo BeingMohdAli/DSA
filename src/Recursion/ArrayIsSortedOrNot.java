9653e91990911213773c75af64a22769267d44a8
BeingMohdAli <mohdalisaad868@gmail.com>
2026-08-21 11:24:02 +0530

    DSA

---
+++ src/Recursion/ArrayIsSortedOrNot.java
@@ -0,0 +1,32 @@
+package Recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayIsSortedOrNot {
+    static void main() {
+        List<Integer> l = new ArrayList<>(Arrays.asList(1, 2));
+
+        boolean sort = sort(l);
+
+        System.out.println(sort);
+    }
+
+    public static boolean sort(List<Integer> l){
+        if(l.size()==1){
+            return true;
+        }
+        int lastELement = l.removeLast();
+        boolean sort = sort(l);
+        if(!sort){
+            return false;
+        }
+
+        if(l.get(l.size()-1)<lastELement){
+            return true;
+
+        }
+        return false;
+    }
+}
--- src/Recursion/countGoodNumbers.java
+++ src/Recursion/countGoodNumbers.java
@@ -1,13 +1,32 @@
 package Recursion;
 
+import java.util.HashMap;
+
 public class countGoodNumbers {
     static void main() {
-
+        System.out.println(goodNumbers(3));
 
     }
 
-    public static int goodNumbers(int n){
-        int digits  =  (int)Math.log10(n)+1;
-        return x;
+    public static int goodNumbers(long n){
+       long evenPosition = (n+1)/2;
+        long oddPosition = n/2;
+
+        long l = myPow(5, evenPosition) * myPow(4, oddPosition);
+        long l1 = l % 1_000_000_007;
+        return (int)l1;
+
+    }
+
+    public static long myPow(long x, long position) {
+        if(position==0){
+            return 1;
+        }
+
+        if(position%2==0){
+            return myPow(x*x,position/2);
+        }
+
+        return x*myPow(x*x,position/2);
     }
 }
