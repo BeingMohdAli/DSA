6e0ee42607ea13d3f386303b484bfcf0c512238c
BeingMohdAli <mohdalisaad868@gmail.com>
2026-08-22 13:45:16 +0530

    DSA

--- src/Recursion/subsets.java
+++ src/Recursion/subsets.java
@@ -1,4 +1,17 @@
 package Recursion;
 
 public class subsets {
+    static void main() {
+        subsetsOfString("abc","");
+    }
+    public static void subsetsOfString(String input,String output){
+        if(input.isEmpty()){
+            System.out.println(output);
+            return;
+        }
+        char c = input.charAt(0);
+        subsetsOfString(input.substring(1),output+c);
+        subsetsOfString(input.substring(1),output);
+
+    }
 }
--- src/Recursion/validCombinationOfParaenthesis.java
+++ src/Recursion/validCombinationOfParaenthesis.java
@@ -1,4 +1,46 @@
-package Recursion;
-
-public class validCombinationOfParaenthesis {
-}
+//class Solution {
+//    public List<String> generateParenthesis(int n) {
+//
+//        List<String> ans = new ArrayList<>();
+//
+//        generateParenthesis("", 0, 0, n, ans);
+//
+//        return ans;
+//    }
+//
+//    public  void generateParenthesis(
+//            String output,
+//            int open,
+//            int close,
+//            int n,
+//            List<String> ans) {
+//
+//        // Base case
+//        if (open == n && close == n) {
+//            ans.add(output);
+//            return;
+//        }
+//
+//        // Choice 1: add '('
+//        if (open < n) {
+//            generateParenthesis(
+//                    output + "(",
+//                    open + 1,
+//                    close,
+//                    n,
+//                    ans
+//            );
+//        }
+//
+//        // Choice 2: add ')'
+//        if (close < open) {
+//            generateParenthesis(
+//                    output + ")",
+//                    open,
+//                    close + 1,
+//                    n,
+//                    ans
+//            );
+//        }
+//    }
+//}
