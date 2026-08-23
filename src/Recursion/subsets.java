fbdff481886c8eb395adefb4934b3547e08a4c0a
BeingMohdAli <mohdalisaad868@gmail.com>
2026-08-23 10:24:05 +0530

    frontend

--- src/Recursion/subsets.java
+++ src/Recursion/subsets.java
@@ -1,17 +1,23 @@
 package Recursion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class subsets {
     static void main() {
-        subsetsOfString("abc","");
+        List<String> l = new ArrayList<>();
+        subsetsOfString("abc","",l);
+        System.out.println(l);
     }
-    public static void subsetsOfString(String input,String output){
+    public static void subsetsOfString(String input, String output, List<String> l){
         if(input.isEmpty()){
-            System.out.println(output);
+          l.add(output);
             return;
         }
         char c = input.charAt(0);
-        subsetsOfString(input.substring(1),output+c);
-        subsetsOfString(input.substring(1),output);
+        subsetsOfString(input.substring(1),output+c,l);
+        subsetsOfString(input.substring(1),output,l);
 
     }
 }
