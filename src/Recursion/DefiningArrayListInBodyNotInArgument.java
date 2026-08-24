82230734a137963fc7b0ae22f954d16e7dc2b013
BeingMohdAli <mohdalisaad868@gmail.com>
2026-08-24 10:30:58 +0530

    DSA

---
+++ src/Recursion/DefiningArrayListInBodyNotInArgument.java
@@ -0,0 +1,29 @@
+package Recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DefiningArrayListInBodyNotInArgument {
+    static void main() {
+        int[] arr = {1,2,3,4,4,5};
+        int target = 4;
+
+        System.out.println(LinearSearch(arr,target,0));
+    }
+    public static List<Integer> LinearSearch(int[] arr,int target,int index){
+        if(index==arr.length){
+            ArrayList<Integer> l = new ArrayList<>();
+            return l;
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        if(arr[index]==target){
+            list.add(index);
+        }
+
+        List<Integer> integers = LinearSearch(arr, target, index + 1);
+        list.addAll(integers);
+        return list;
+
+    }
+}
