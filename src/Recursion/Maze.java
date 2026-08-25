f5eb2526d6f841dc22390e4c4ea277dc1f4ebcbf
BeingMohdAli <mohdalisaad868@gmail.com>
2026-08-25 10:36:45 +0530

    DSA

--- src/Recursion/Maze.java
+++ src/Recursion/Maze.java
@@ -5,8 +5,9 @@ import java.util.List;
 
 public class Maze {
     static void main() {
-//        System.out.println(paths(3,3));
         System.out.println(pathToReachTheEndOf3by3grid("",3,3));
+//        System.out.println(paths(3,3));
+//        System.out.println(pathToReachTheEndOf3by3grid("",3,3));
     }
 
     //    public static int paths(int r , int c){
@@ -21,7 +22,26 @@ public class Maze {
 
 //
 //    }
-    public static List<String> pathToReachTheEndOf3by3grid(String pro, int r, int c) {
+//    public static List<String> pathToReachTheEndOf3by3grid(String pro, int r, int c) {
+//        if (r == 1 && c == 1) {
+//            List<String> list = new ArrayList<>();
+//            list.add(pro);
+//            return list;
+//
+//        }
+//        ArrayList<String> s = new ArrayList<>();
+//        if (r > 1) {
+//            List<String> s1 = pathToReachTheEndOf3by3grid(pro + "D", r - 1, c);
+//            s.addAll(s1);
+//        }
+//        if (c > 1) {
+//            List<String> s2 = pathToReachTheEndOf3by3grid(pro + "R", r, c - 1);
+//            s.addAll(s2);
+//        }
+//
+//        return s;
+//    }
+        public static List<String> pathToReachTheEndOf3by3grid(String pro, int r, int c) {
         if (r == 1 && c == 1) {
             List<String> list = new ArrayList<>();
             list.add(pro);
@@ -33,6 +53,10 @@ public class Maze {
             List<String> s1 = pathToReachTheEndOf3by3grid(pro + "D", r - 1, c);
             s.addAll(s1);
         }
+        if(r>1&&c>1){
+            List<String> s3 = pathToReachTheEndOf3by3grid(pro + "S", r - 1, c-1);
+            s.addAll(s3);
+        }
         if (c > 1) {
             List<String> s2 = pathToReachTheEndOf3by3grid(pro + "R", r, c - 1);
             s.addAll(s2);
@@ -40,4 +64,7 @@ public class Maze {
 
         return s;
     }
+
+
+
 }
