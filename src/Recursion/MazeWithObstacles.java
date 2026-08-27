74517219bbf27a46ed3e26349075d580359aaf50
BeingMohdAli <mohdalisaad868@gmail.com>
2026-08-27 14:25:34 +0530

    DSA

--- src/Recursion/MazeWithObstacles.java
+++ src/Recursion/MazeWithObstacles.java
@@ -5,36 +5,37 @@ import java.util.List;
 
 public class MazeWithObstacles {
     static void main() {
-        System.out.println(mazeWithO("",0,0));
+        boolean[][] grid = {
+                {true, true, true},
+                {true, false, true},
+                {true, true, true}
+        };
+        System.out.println(mazeWithObstacles("",0,0,grid));
+
     }
 
-    public static List<String> mazeWithO(String pro, int r, int c) {
-        if (r == 2 && c == 2) {
-            List<String> list = new ArrayList<>();
-            list.add(pro);
-            return list;
+    public static List<String> mazeWithObstacles(String p,int r, int c, boolean[][] arr){
 
+        if(!arr[r][c]){
+            List<String> l = new ArrayList<>();
+            return l;
+        }
+        if(r==arr.length-1&&c==arr[0].length-1){
+            List<String> l = new ArrayList<>();
+            l.add(p);
+            return l;
         }
 
-        if(r==1 &&c==1){
-            List<String> list = new ArrayList<>();
-            return list;
+        ArrayList<String> ans = new ArrayList<>();
+        if(r<arr.length-1){
+             ans.addAll(mazeWithObstacles(p + "D", r + 1, c, arr));
         }
 
-
-        ArrayList<String> s = new ArrayList<>();
-        if (r <2) {
-            List<String> s1 = mazeWithO(pro + "D", r  + 1, c);
-            s.addAll(s1);
+        if(c<arr[0].length-1){
+            ans.addAll(mazeWithObstacles(p + "R", r , c+1, arr));
         }
+            return ans;
 
-        if (c < 2) {
-            List<String> s2 = mazeWithO(pro + "R", r, c + 1);
-            s.addAll(s2);
-        }
-
-        return s;
     }
-
 
 }
