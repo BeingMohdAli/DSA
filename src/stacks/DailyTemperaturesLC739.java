e463ae554ca18db203235ecfd81ac55f51d91a07
BeingMohdAli <mohdalisaad868@gmail.com>
2026-09-14 23:44:55 +0530

    DSA

--- src/stacks/DailyTemperaturesLC739.java
+++ src/stacks/DailyTemperaturesLC739.java
@@ -1,13 +1,11 @@
 package stacks;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class DailyTemperaturesLC739 {
     static void main() {
-        int[] arr = {30,40,50,60};
-        System.out.println(dT(arr));
+        int[] arr = {73, 74, 75, 71, 69, 72, 76, 73};
+        System.out.println(Arrays.toString(dT(arr)));
     }
 
 //    public static int[] dT(int[] arr){
@@ -37,30 +35,23 @@ public class DailyTemperaturesLC739 {
 //
 //    }
 
-    public static List<Integer> dT(int[] arr) {
-
-        Stack<Integer>  s = new Stack<>();
-        List<Integer> l = new ArrayList<>();
-
-        for (int i = arr.length-1; i >=0; i--) {
-            if(s.isEmpty()){
-               s.push(i);
-               l.add(0);
-            }
-
-            else if(arr[i]>s.peek()){
-                l.add(0);
+    public static int[] dT(int[] arr) {
+        Stack<Integer> s = new Stack<>();
+        int[] result = new int[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!s.isEmpty() && arr[i] >= arr[s.peek()]) {
                 s.pop();
-            }else{
-                s.push(i);
-                l.add(s.peek()-i);
-
             }
+            if (s.isEmpty()) {
+                result[i] = 0;
+            } else {
+                result[i] = s.peek()-i;
+            }
+            s.push(i);
         }
-        return l;
+
+        return result;
+
 
     }
-
-
-
 }
